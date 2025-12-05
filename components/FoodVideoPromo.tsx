963f59781b2e8c511bb1a5c3c16d7061a3348140
chinthakabandara710 <77668640+chinthakabandara710@users.noreply.github.com>
2025-12-05 14:17:35 +0530

    Update FoodVideoPromo.tsx

--- components/FoodVideoPromo.tsx
+++ components/FoodVideoPromo.tsx
@@ -286,7 +286,7 @@ export default function FoodVideoPromoSection() {
           </motion.div>
           {/* right section */}
           <motion.div
-            className="relative w-3/7 h-90 bg-emerald-400 rounded-3xl p-2 shadow-2xl "
+            className="relative w-3/7 h-90 rounded-3xl p-2 shadow-2xl bg-[linear-gradient(to_bottom_right,#0e685b,#10b981,#0e685b)] "
             initial={{ opacity: 0, x: 50 }}
             whileInView={{ opacity: 1, x: 0 }}
             viewport={{ once: true }}
